6c0538cbda9eb16a1367cb81bc7e5969cbd818a5
Priyanshu <priyanshu.bansal@extramarks.com>
2025-07-06 22:55:00 +0530

    Fix: Added heading

--- server/src/services/linkedinPost.js
+++ server/src/services/linkedinPost.js
@@ -2,19 +2,29 @@ const { callDeepSeek } = require("../utils/Deepseek");
 const { getNewTopic } = require("../utils/getNewTopic");
 const { createTextPost } = require("../utils/LinkedinAPI");
 const { callOpenAI } = require("../utils/OpenAI");
-
+const dayjs = require("dayjs");
 
 const getGeneratedPost = async () => {
+  const START_DATE = process.env.START_DATE;
+  const today = dayjs();
+  const startDate = dayjs(START_DATE);
+  const dayNumber = today.diff(startDate, "day") + 1;
   const prompt = process.env.PROMPT.trim();
   const newTopic = getNewTopic();
   const fullPrompt = `Generate a LinkedIn post ABOUT ${newTopic} EXCLUSIVELY. Follow these rules: ${prompt}`;
   const response = await callDeepSeek(fullPrompt);
-  if(!response || response.length === 0) {
+  if (!response || response.length === 0) {
     throw new Error("Failed to generate post content");
   }
-  const post = await createTextPost(response, process.env.LINKEDIN_ACCESS_TOKEN);
+
+  const finalLinkedInPost = `👋 Hey LinkedIn! Day ${dayNumber} of my AI series is here.\nToday’s topic: ${newTopic}\n\n${response}`;
+
+  const post = await createTextPost(
+    finalLinkedInPost,
+    process.env.LINKEDIN_ACCESS_TOKEN
+  );
   return post;
-}; 
+};
 
 module.exports = {
   getGeneratedPost,
